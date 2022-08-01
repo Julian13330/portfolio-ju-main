1e8b85522340d94c8172591478e22abbef1046fb
Julian13330 <juliancal@hotmail.fr>
2022-08-01 18:14:18 +0200

    Ajout typeswriter librairie

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ window.onscroll=function(){
         
     }
 }
+
+// Effet machine à écrire dans la page d'accueil 
+
+const typeWriter = document.getElementById('type-writer');
+
+let typewriter = new Typewriter(typeWriter, {
+  loop: true
+});
+
+typewriter.typeString('Freelance')
+  .pauseFor(1500)
+  .deleteChars(9)
+  .typeString('<span style="color: #FF5722;">HTML 5</span>')
+  .pauseFor(1000)
+  .deleteChars(6)
+  .typeString('<span style="color: #2196F3;">CSS 3</span>')
+  .pauseFor(1000)
+  .deleteChars(5)
+  .typeString('<span style="color: #FFDF00;">JavaScript</span>')
+  .pauseFor(1000)
+  .deleteChars(10)
+  .typeString('<span style="color: #777BB3;">Php</span>')
+  .pauseFor(1000)
+  .deleteChars(3)
+  .typeString('<span style="color: #fff;">Symfony</span>')
+  .pauseFor(1000)
+  .start();
